e2d22672cc3bafdcaa51a6bc73f462e99a5fa48b
sylvainlapeyrade <sylvain.lapeyrade@hotmail.fr>
2019-12-09 18:05:32 +0100

    task1

--- TDDE13_Lab2-Multi-agent_Reinforcement_Learning_Lab/exercice1_frozenlake.py
+++ TDDE13_Lab2-Multi-agent_Reinforcement_Learning_Lab/exercice1_frozenlake.py
@@ -14,25 +14,29 @@ class Agent:
     def act(self, state):
         # Select action for exploitation or exploitation
         # YOUR CODE HERE
-        action = 0
-        if np.random.uniform(0, 1) < epsilon:
+        # The greater Epsilon (in the first expisodes), the more chances
+        # there are of a randomly selected action
+        # Otherwise (last expisodes) take the best action from the q_table
+        if np.random.uniform(0, 1) < self.epsilon:
             action = env.action_space.sample()
         else:
-            action = np.argmax(self.q_table[state, action])
+            action = np.argmax(self.q_table[state, :])
         return action
 
     def learn(self, state, action, reward, next_state):
         # Update Q table based on experience
         # YOUR CODE HERE
-        self.q_table[state, action] = self.q_table[state, action] + self.alpha(
-            reward + self.epsilon * np.max(self.q_table[next_state, action])
+        # Implementation of the Q-learning formula
+        self.q_table[state, action] = self.q_table[state, action] + \
+            self.alpha * (reward + self.gamma * np.max(
+                self.q_table[next_state, :])
             - self.q_table[state, action])
 
 
 env = gym.make('FrozenLake-v0')
 
-alpha = 0.1  # YOUR CODE HERE
-gamma = 0.99  # YOUR CODE HERE
+alpha = 0.9  # YOUR CODE HERE
+gamma = 0.9  # YOUR CODE HERE
 epsilon = 0.9  # YOUR CODE HERE
 
 my_agent = Agent(env.action_space, (env.observation_space.n,
@@ -61,7 +65,10 @@ for i in range(num_expisodes):
         if done:
             break
 
-    if(done):  # YOUR CODE HERE
-        my_agent.epsilon = 1  # YOUR CODE HERE
-        total_reward += ep_reward
-        print(total_reward/num_expisodes)
+    # Decrease slowly epsilon every expisodes to regulate the learning
+    if(my_agent.epsilon > 0):  # YOUR CODE HERE
+        my_agent.epsilon -= 0.001  # YOUR CODE HERE
+    total_reward += ep_reward
+print("Accumulated reward:", total_reward / num_expisodes)
+print("\nBest value:", np.argmax(my_agent.q_table, axis=1))
+print("Q-Table values:\n", my_agent.q_table)
